4be1b11f81140a16bc1ff326dc26efb57404398f
sergeychama24 <sergeychama24@gmail.com>
2024-03-18 00:07:46 +0300

    fix layout

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const gameTitle = document.querySelector('.hero__title');
 const gameMainGenre = document.querySelector('.hero__genre--short');
 const gameScore = document.querySelector('.hero__score');
 const screenshots = document.querySelectorAll('.screenshot');
+const gameGenres = document.querySelector('.genres__list');
 
 const initialGame = (gameId) => {
   getGameByIdRequest(gameId).then((data) => {
@@ -17,18 +18,24 @@ const initialGame = (gameId) => {
     gameScore.textContent = data.metacritic;
     gameCover.src = data.background_image;
     gameCover.alt = data.name;
+    data.genres.forEach((genre) => {
+      const gameGenre = document.createElement('li');
+      gameGenre.classList.add('genres__list-item');
+      gameGenre.textContent = genre.name;
+      gameGenres.prepend(gameGenre);
+    });
+
     console.log(data);
   });
 
   getScreenshotsByIdRequest(gameId).then((data) => {
-    console.log(data);
     screenshots.forEach((screenshot, index) => {
       screenshot.src = data.results[index].image;
-      screenshot.alt = data.results[index].name;
+      screenshot.alt = `Screenshot`;
     });
   });
 };
 
-initialGame('persona-5');
+initialGame('far-cry-5');
 
 console.log(screenshots);
