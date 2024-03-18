b48579508a72ea5014cfe8622dc5db105ec9aed1
sergeychama24 <sergeychama24@gmail.com>
2024-03-18 08:25:46 +0300

    add platforms block

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ const screenshots = document.querySelectorAll('.screenshot');
 const gameGenres = document.querySelector('.genres__list');
 const description = document.querySelector('.description');
 const released = document.querySelector('.released');
+const developers = document.querySelector('.developers');
+const platforms = document.querySelector('.platforms__list');
 
 const initialGame = (gameId) => {
   getGameByIdRequest(gameId).then((data) => {
@@ -26,16 +28,53 @@ const initialGame = (gameId) => {
     gameDescriptionText.textContent = data.description_raw;
     description.append(gameDescriptionText);
 
+    function monthToString(month) {
+      const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ];
+      return months[month - 1];
+    }
+
+    function formatDate(date) {
+      const parts = date.split('-');
+      const year = parts[0];
+      const month = parseInt(parts[1]);
+      const day = parts[2];
+      const monthInWords = monthToString(month);
+      return `${day} ${monthInWords} ${year}`;
+    }
+
+    const releasedDate = data.released;
+    const releasedString = formatDate(releasedDate);
+
     const gameReleasedText = document.createElement('p');
     gameReleasedText.classList.add('released__text');
-    gameReleasedText.textContent = data.released;
+    gameReleasedText.textContent = releasedString;
     released.append(gameReleasedText);
 
     data.genres.forEach((genre) => {
       const gameGenre = document.createElement('li');
       gameGenre.classList.add('genres__list-item');
       gameGenre.textContent = genre.name;
-      gameGenres.prepend(gameGenre);
+      gameGenres.append(gameGenre);
+    });
+
+    data.platforms.forEach((el) => {
+      const platform = document.createElement('li');
+      platform.classList.add('platforms__list-item');
+      platform.textContent = el.platform.name;
+      platforms.append(platform);
     });
 
     console.log(data);
@@ -49,6 +88,6 @@ const initialGame = (gameId) => {
   });
 };
 
-initialGame('the-legend-of-zelda-breath-of-the-wild');
+initialGame('far-cry-3');
 
 console.log(screenshots);
